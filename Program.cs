251530c4585673ea7df7bf2d09a38918f60e4eb2
VeronikaG <gavrilova.veronika38d@gmail.com>
2026-09-13 19:16:52 +0300

    Lab03 task 1: even or odd check

--- Program.cs
+++ Program.cs
@@ -132,3 +132,14 @@ Console.WriteLine($"Хороший экзамен (>= 60): {hasGoodExam}");
 Console.WriteLine($"Льгота: {hasBenefit}");
 Console.WriteLine($"Проходит по правилам: {isEligibleByRules}");
 Console.WriteLine($"Итоговый балл: {totalScore}");
+
+
+Console.WriteLine();
+Console.WriteLine("Чётное или нечётное");
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+bool isEven = number % 2 == 0;
+
+Console.WriteLine($"Число {number} чётное: {isEven}");
